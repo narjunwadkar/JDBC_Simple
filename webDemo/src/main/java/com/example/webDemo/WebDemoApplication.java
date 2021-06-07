5627f49be78949da115f3d3bf1a43cfd16e1bcda
Narayan Arjunwadkar <narjunwadkar@gmail.com>
2021-06-07 08:29:01 +0530

    7th June Commit

--- webDemo/src/main/java/com/example/webDemo/WebDemoApplication.java
+++ webDemo/src/main/java/com/example/webDemo/WebDemoApplication.java
@@ -1,20 +1,23 @@
 package com.example.webDemo;
 
+import org.apache.catalina.core.ApplicationContext;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
 @ComponentScan(basePackages = "com.example")
 public class WebDemoApplication {
 	
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-	      return application.sources(WebDemoApplication.class);
-	   }
-
-
+	
 	public static void main(String[] args) {
+		
+		ApplicationContext ctx = (ApplicationContext) SpringApplication.run(WebDemoApplication.class, args);
+		
 		SpringApplication.run(WebDemoApplication.class, args);
 		
 		System.out.println("Start....!");
